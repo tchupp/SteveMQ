d3585e518001e6eb02e19f0440bad2fb6d3eebfa
chance <chance.a.cyphers@gmail.com>
2019-09-25 13:18:10 -0400

    Added handler to echo tcp messages

--- apps/broker/lib/broker.ex
+++ apps/broker/lib/broker.ex
@@ -1,18 +1,43 @@
 defmodule Broker do
-  @moduledoc """
-  Documentation for Broker.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
-  ## Examples
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
 
-      iex> Broker.hello()
-      :world
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
 
-  """
-  def hello do
-    :world
+    {:ok, pid} = Task.Supervisor.start_child(Broker.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    msg = read_line(socket)
+
+    Logger.info("Received: stuff")
+    Logger.info("Received: #{msg}")
+
+    write_line(socket, msg)
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    :gen_tcp.recv(socket, 0)
+  end
+
+  defp write_line(socket, {:ok, text}) do
+    :gen_tcp.send(socket, text)
+  end
+
+  defp write_line(socket, {:error, error}) do
+    :gen_tcp.send(socket, "ERROR\r\n")
+    exit(error)
   end
 end
