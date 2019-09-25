f32832f035a970d90da55ec27f4049b4e9262bec
chance <chance.a.cyphers@gmail.com>
2019-09-25 19:29:43 -0400

    Made it CONNACK

--- apps/broker/test/broker_test.exs
+++ apps/broker/test/broker_test.exs
@@ -1,4 +1,27 @@
 defmodule BrokerTest do
   use ExUnit.Case
-  doctest Broker
+
+  setup do
+    Application.stop(:broker)
+    :ok = Application.start(:broker)
+  end
+
+  setup do
+    opts = [:binary, active: false]
+    {:ok, socket} = :gen_tcp.connect('localhost', 1883, opts)
+    %{socket: socket}
+  end
+
+  test "CONNECT returns a CONNACK", %{socket: socket} do
+    connect = <<12>>
+    connack = <<32, 2, 0, 0>>
+
+    assert send_and_recv(socket, connect) == connack
+  end
+
+  defp send_and_recv(socket, packet) do
+    :ok = :gen_tcp.send(socket, packet)
+    {:ok, data} = :gen_tcp.recv(socket, 0, 1000)
+    data
+  end
 end
