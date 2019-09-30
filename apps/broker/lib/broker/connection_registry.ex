d32bdc6232072a1c39cfa6e471c3009097f5cedd
chance <chance.a.cyphers@gmail.com>
2019-09-30 19:31:50 -0400

    Add a registry for connections

---
+++ apps/broker/lib/broker/connection_registry.ex
@@ -0,0 +1,20 @@
+defmodule Broker.ConnectionRegistry do
+  use Agent
+
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  def get_pid(registry, client_id) do
+    Agent.get(registry, & &1[client_id])
+  end
+
+  def register(registry, client_id, pid) do
+    Agent.update(registry, &Map.put(&1, client_id, pid))
+  end
+
+  def remove(registry, client_id) do
+    Agent.get_and_update(registry, &Map.pop(&1, client_id))
+  end
+
+end
