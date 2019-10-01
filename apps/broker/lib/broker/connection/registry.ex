fd87b8b6c2e3f12ffa8cd6fa822109aea10939b9
chance <chance.a.cyphers@gmail.com>
2019-10-01 18:48:41 -0400

    refactored registry from Agent -> GenServer

--- apps/broker/lib/broker/connection/registry.ex
+++ apps/broker/lib/broker/connection/registry.ex
@@ -1,19 +1,45 @@
 defmodule Broker.Connection.Registry do
-  use Agent
+  use GenServer
+  require Logger
 
-  def start_link(_opts) do
-    Agent.start_link(fn -> %{} end)
+  def start_link(opts) do
+    server = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server, opts)
   end
 
   def get_pid(registry, client_id) do
-    Agent.get(registry, & &1[client_id])
+    GenServer.call(registry, {:get_pid, client_id})
   end
 
   def register(registry, client_id, pid) do
-    Agent.update(registry, &Map.put(&1, client_id, pid))
+    GenServer.call(registry, {:register, client_id, pid})
   end
 
   def remove(registry, client_id) do
-    Agent.get_and_update(registry, &Map.pop(&1, client_id))
+    GenServer.call(registry, {:remove, client_id})
   end
+
+  @impl true
+  def init(_opts) do
+    {:ok, {%{}, %{}}}
+  end
+
+  @impl true
+  def handle_call({:get_pid, client_id}, _from, state) do
+    {clients_to_pids, _} = state
+    pid = Map.get(clients_to_pids, client_id)
+    {:reply, pid, state}
+  end
+
+  @impl true
+  def handle_call({:register, client_id, pid}, _from, {client_id_map, monitor_refs}) do
+    {:reply, :ok, {Map.put(client_id_map, client_id, pid), monitor_refs}}
+  end
+
+  @impl true
+  def handle_call({:remove, client_id}, _from, {clients_to_pids, monitor_refs}) do
+    {_, new_map} = Map.pop(clients_to_pids, client_id)
+    {:reply, :ok, {new_map, monitor_refs}}
+  end
+
 end
