ad733eb2e5f64cc7ac5f4d02c07bf13d1ef4cf08
chance <chance.a.cyphers@gmail.com>
2019-10-01 23:21:16 -0400

    added ability to remove subscriptions

--- apps/broker/test/broker/subscription_registry_test.exs
+++ apps/broker/test/broker/subscription_registry_test.exs
@@ -17,4 +17,26 @@ defmodule Broker.SubscriptionRegistryTest do
     assert Enum.at(clients, 0) == "client1"
     assert Enum.at(clients, 1) == "client2"
   end
+
+  test "removes client ids from topics", %{registry: registry} do
+    Broker.SubscriptionRegistry.add_subscription(registry, "client1", "b/topic")
+    Broker.SubscriptionRegistry.add_subscription(registry, "client2", "b/topic")
+
+    Broker.SubscriptionRegistry.remove_subscription(registry, "client1", "b/topic")
+
+    clients = Broker.SubscriptionRegistry.get_subscribers(registry, "b/topic")
+    assert length(clients) == 1
+    assert Enum.at(clients, 0) == "client2"
+  end
+
+  test "can add to subscriptions that were previously emptied", %{registry: registry} do
+    Broker.SubscriptionRegistry.add_subscription(registry, "client1", "c/topic")
+    Broker.SubscriptionRegistry.remove_subscription(registry, "client1", "c/topic")
+
+    Broker.SubscriptionRegistry.add_subscription(registry, "client2", "c/topic")
+
+    clients = Broker.SubscriptionRegistry.get_subscribers(registry, "c/topic")
+    assert length(clients) == 1
+    assert Enum.at(clients, 0) == "client2"
+  end
 end
