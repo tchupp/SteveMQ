7cd055ba57bfbab42ee08b99cc79c9b9596f779f
chance <chance.a.cyphers@gmail.com>
2019-10-04 18:37:44 -0400

    move packet parsing to a decode module

--- apps/broker/test/packet/decode_test.exs
+++ apps/broker/test/packet/decode_test.exs
@@ -1,8 +1,8 @@
-defmodule Broker.PacketTest do
+defmodule Packet.DecodeTest do
   use ExUnit.Case
 
   test "returns error for unrecognized message types" do
-    {result, _} = Broker.Packet.parse(<<0, 2, 0, 0>>)
+    {result, _} = Packet.Decode.parse(<<0, 2, 0, 0>>)
     assert result == :error
   end
 
@@ -11,7 +11,7 @@ defmodule Broker.PacketTest do
       <<16, 24, 0, 4, ?M, ?Q, ?T, ?T, 5, 2, 0, 60, 0, 0, 11, ?h, ?e, ?l, ?l, ?o, 32, ?w, ?o, ?r,
         ?l, ?d>>
 
-    {result, data} = Broker.Packet.parse(connect)
+    {result, data} = Packet.Decode.parse(connect)
 
     assert result == :connect
     assert data[:client_id] == "hello world"
@@ -23,7 +23,7 @@ defmodule Broker.PacketTest do
   test "parses SUBSCRIBE" do
     packet_id = 123
     subscribe = <<130, 14, packet_id::16, 0, 0, 9, ?t, ?e, ?s, ?t, ?T, ?o, ?p, ?i, ?c, 0>>
-    {result, packet} = Broker.Packet.parse(subscribe)
+    {result, packet} = Packet.Decode.parse(subscribe)
 
     assert result == :subscribe
     assert packet[:topic_filter] == "testTopic"
@@ -35,35 +35,35 @@ defmodule Broker.PacketTest do
       <<3::4, 0::4>> <>
         <<14>> <> <<0, 5, ?t, ?o, ?p, ?i, ?c>> <> <<0>> <> <<?m, ?e, ?s, ?s, ?a, ?g, ?e>>
 
-    {_, packet} = Broker.Packet.parse(publish)
+    {_, packet} = Packet.Decode.parse(publish)
 
     assert packet[:topic] == "topic"
     assert packet[:message] == "message"
   end
 
   test "can parse one length variable length ints" do
-    assert Broker.Packet.parse_variable_int(<<0, 0>>) == {0, <<0>>}
-    assert Broker.Packet.parse_variable_int(<<127, 0>>) == {127, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<0, 0>>) == {0, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<127, 0>>) == {127, <<0>>}
   end
 
   test "can parse two length variable length ints" do
-    assert Broker.Packet.parse_variable_int(<<128, 1, 0>>) == {128, <<0>>}
-    assert Broker.Packet.parse_variable_int(<<255, 127, 0>>) == {16_383, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<128, 1, 0>>) == {128, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<255, 127, 0>>) == {16_383, <<0>>}
   end
 
   test "can parse three length variable length ints" do
-    assert Broker.Packet.parse_variable_int(<<128, 128, 1, 0>>) == {16_384, <<0>>}
-    assert Broker.Packet.parse_variable_int(<<255, 255, 127, 0>>) == {2_097_151, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<128, 128, 1, 0>>) == {16_384, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<255, 255, 127, 0>>) == {2_097_151, <<0>>}
   end
 
   test "can parse four length variable length ints" do
-    assert Broker.Packet.parse_variable_int(<<128, 128, 128, 1, 0>>) == {2_097_152, <<0>>}
-    assert Broker.Packet.parse_variable_int(<<255, 255, 255, 127, 0>>) == {268_435_455, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<128, 128, 128, 1, 0>>) == {2_097_152, <<0>>}
+    assert Packet.Decode.parse_variable_int(<<255, 255, 255, 127, 0>>) == {268_435_455, <<0>>}
   end
 
   test "max variable length bytes is 4" do
     assert_raise RuntimeError, ~r/error/, fn ->
-      Broker.Packet.parse_variable_int(<<255, 255, 255, 255, 7>>)
+      Packet.Decode.parse_variable_int(<<255, 255, 255, 255, 7>>)
     end
   end
 end
