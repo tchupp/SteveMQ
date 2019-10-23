997dcf20eba989652ae7579b4f946987027ebb74
chance <chance.a.cyphers@gmail.com>
2019-10-23 18:56:32 -0400

    add a new app for the client library

---
+++ apps/client/lib/client.ex
@@ -0,0 +1,18 @@
+defmodule Client do
+  @moduledoc """
+  Documentation for Client.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Client.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
