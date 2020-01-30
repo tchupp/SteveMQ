8435dbf46468f3c7f3a0da69527a6d3fa6d84b0a
theochupp <tclchiam@gmail.com>
2020-01-30 00:13:06 -0500

    feat: add receiver that reads from a tcp socket until it has full packets

---
+++ apps/client/test/connection/receiver_test.exs
@@ -0,0 +1,84 @@
+defmodule Connection.ReceiverTest do
+  use ExUnit.Case
+  doctest Connection.Receiver
+
+  alias Connection.Receiver
+
+  setup context do
+    _ = start_supervised!({Registry, keys: :unique, name: Client.Bucket})
+
+    {:ok, %{client_id: context.test}}
+  end
+
+  def setup_connection(context) do
+    {:ok, client_socket, server_socket} = TestHelper.FakeTCPThing.new()
+    {:ok, Map.merge(context, %{client: client_socket, server: server_socket})}
+  end
+
+  defp drop_connection(%{server: server} = context) do
+    :ok = :gen_tcp.close(server)
+    {:ok, Map.drop(context, [:client, :server])}
+  end
+
+  def setup_receiver(context) do
+    {:ok, pid} = Receiver.start_link(client_id: context.client_id, socket: context.client)
+    :ok = Receiver.handle_socket(pid, context.client)
+    {:ok, %{receiver_pid: pid}}
+  end
+
+  def setup_client(context) do
+    Registry.register(Client.Bucket, {Client, context.client_id}, self())
+    :ok
+  end
+
+  describe "receiving things" do
+    setup [:setup_connection, :setup_receiver, :setup_client]
+
+    test "receive a publish with a small payload", context do
+      message = :crypto.strong_rand_bytes(1448)
+      publish = %Packet.Publish{packet_id: 1, qos: 1, topic: "another/one", message: message}
+
+      encoded_publish = Packet.encode(publish)
+      :ok = :gen_tcp.send(context.server, encoded_publish)
+
+      assert_receive {:"$gen_call", caller, {:receive_packet, ^encoded_publish}}
+    end
+
+    test "receive a publish with a big ol payload", context do
+      message = :crypto.strong_rand_bytes(120_000)
+      publish = %Packet.Publish{packet_id: 1, qos: 1, topic: "another/one", message: message}
+
+      encoded_publish = Packet.encode(publish)
+      :ok = :gen_tcp.send(context.server, encoded_publish)
+
+      assert_receive {:"$gen_call", caller, {:receive_packet, ^encoded_publish}}
+    end
+
+    test "very slow connection", context do
+      Process.flag(:trap_exit, true)
+      receiver_pid = context.receiver_pid
+
+      # make sure we don't crash the receiver by sending one byte.....then waiting
+      :ok = :gen_tcp.send(context.server, <<0b11010000>>)
+      refute_receive {:EXIT, ^receiver_pid, {:protocol_violation, :invalid_header_length}}, 400
+
+      # send the second byte and make sure we get the right packet
+      :ok = :gen_tcp.send(context.server, <<0>>)
+      assert_receive {:"$gen_call", caller, {:receive_packet, data}}, 10000
+      assert {:pingresp, %Packet.Pingresp{}} = Packet.decode(data)
+    end
+  end
+
+  describe "bad packets" do
+    setup [:setup_connection, :setup_receiver]
+
+    test "invalid header length", context do
+      Process.flag(:trap_exit, true)
+      receiver_pid = context.receiver_pid
+      # send more bytes than the fixed header size
+      # the header parser should return protocol violation
+      :ok = :gen_tcp.send(context.server, <<1, 255, 255, 255, 255, 0>>)
+      assert_receive {:EXIT, ^receiver_pid, {:protocol_violation, :invalid_header_length}}
+    end
+  end
+end
